2ad6d4be204680a75b5534d9ab7157b4dff2b5f5
Conor Owens <conorj.owens@gmail.com>
2019-03-24 02:25:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,10 +28,11 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array = [1, 2, 3]
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
+  array = [1, 2, 3]
   
 end
 
